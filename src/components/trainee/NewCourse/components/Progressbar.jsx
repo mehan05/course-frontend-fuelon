40fbf871fa9b3a510a635a36a8a12ed4d16ce12e
mehan05 <mehanmehan6@gmail.com>
2024-11-18 15:41:09 +0530

    corrected new course pages

--- src/components/trainee/NewCourse/components/Progressbar.jsx
+++ src/components/trainee/NewCourse/components/Progressbar.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 
-function ProgressBar() {
+function ProgressBar({ precentage }) {
   const [progress, setProgress] = useState(0);
 
- 
+  useEffect(() => {
+    setProgress(precentage);
+  }, [precentage]);
 
   return (
-    <div className="bg-gray-300 rounded-full h-2">
+    <div className="bg-gray-300 rounded-full h-2 overflow-hidden">
       <div
-        className="bg-violet-500 h-full rounded-full"
-        style={{ width: `${30}%` }}
+        className="bg-violet-500 h-full rounded-full transition-[width] duration-1000 ease-in-out"
+        style={{ width: `${progress}%` }}
       />
     </div>
   );
